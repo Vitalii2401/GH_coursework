ac56c7bb091208ee5c352764c31e34eccb3bbd57
Vitalii <movsesyan.vitaliy24ua@gmail.com>
2022-05-18 21:17:24 +0300

    Refactor: create fragment in activity.

--- app/src/main/java/com/example/news/ui/MainActivity.kt
+++ app/src/main/java/com/example/news/ui/MainActivity.kt
@@ -3,10 +3,20 @@ package com.example.news.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.news.R
+import com.example.news.ui.news_list.NewsListFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragment = NewsListFragment()
+
+        if(savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.newsFrameLayout, fragment)
+                .commit()
+        }
     }
 }
