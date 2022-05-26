345535c96634ad64a405d06ce4ff8e13d678d7f9
Vitalii <movsesyan.vitaliy24ua@gmail.com>
2022-05-26 11:52:44 +0300

    Open tabs fragment and add bootom nav.

--- app/src/main/java/com/example/news/ui/tabs/TabsFragment.kt
+++ app/src/main/java/com/example/news/ui/tabs/TabsFragment.kt
@@ -5,15 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.news.R
+import com.example.news.databinding.FragmentTabsBinding
 
 class TabsFragment : Fragment() {
 
+    private lateinit var binding: FragmentTabsBinding
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tabs, container, false)
+        binding = FragmentTabsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navHost = childFragmentManager.findFragmentById(R.id.tabsContainer) as NavHostFragment
+        val navController = navHost.navController
+
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
     }
 }
