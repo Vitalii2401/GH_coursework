4da5b11ddd7bb230e6175fd1ea910590a772d4f2
Vitalii <movsesyan.vitaliy24ua@gmail.com>
2022-05-26 13:26:52 +0300

    Show user profile and sign out account.

--- app/src/main/java/com/example/news/ui/tabs/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/news/ui/tabs/profile/ProfileFragment.kt
@@ -1,19 +1,56 @@
 package com.example.news.ui.tabs.profile
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.example.news.R
+import com.example.news.databinding.FragmentProfileBinding
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.app
 
 class ProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
+    private val firebaseUser = Firebase.auth.currentUser
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        updateUi()
+        logOut()
+    }
+
+    private fun updateUi() {
+        Glide.with(binding.userImage.context)
+            .load(firebaseUser?.photoUrl)
+            .circleCrop()
+            .into(binding.userImage)
+
+        binding.userName.text = firebaseUser?.displayName
+        binding.userMail.text = firebaseUser?.email
+    }
+
+    private fun logOut() {
+        binding.signOutButton.setOnClickListener {
+            AuthUI.getInstance().signOut(requireContext()).addOnSuccessListener {
+                Log.d("Click logout", "logOut: true")
+                findNavController().navigate(R.id.signInFragment)
+            }
+        }
     }
 }
