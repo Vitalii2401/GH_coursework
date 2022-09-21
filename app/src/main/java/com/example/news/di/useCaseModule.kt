9558ceadb7324a89d5a93f70a1a18ffb621c6202
Vitalii <movsesyan,vitaliy24ua@gmail.com>
2022-09-21 17:11:56 +0300

    Added use cases to the module

--- app/src/main/java/com/example/news/di/useCaseModule.kt
+++ app/src/main/java/com/example/news/di/useCaseModule.kt
@@ -1,10 +1,16 @@
 package com.example.news.di
 
+import com.example.news.domain.usecase.AddNewsToBookmarksUseCase
+import com.example.news.domain.usecase.DeleteNewsFromBookmarksUseCase
+import com.example.news.domain.usecase.GetListBookmarksUseCase
 import com.example.news.domain.usecase.GetNewsListUseCase
 import org.koin.dsl.module
 
 val useCaseModule = module {
     factory {
         GetNewsListUseCase(newsRepository = get())
+        AddNewsToBookmarksUseCase(newsRepository = get())
+        DeleteNewsFromBookmarksUseCase(newsRepository = get())
+        GetListBookmarksUseCase(newsRepository = get())
     }
 }
