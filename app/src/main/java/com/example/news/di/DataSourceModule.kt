7465ab18671a440a211b1f6922c7e1ce0d102382
Vitalii <movsesyan,vitaliy24ua@gmail.com>
2022-09-21 17:00:21 +0300

    Added dependencies in the module

--- app/src/main/java/com/example/news/di/DataSourceModule.kt
+++ app/src/main/java/com/example/news/di/DataSourceModule.kt
@@ -3,6 +3,7 @@ package com.example.news.di
 import com.example.news.data.api.datasource.NewsRemoteDataSourceImpl
 import com.example.news.data.database.datasource.NewsLocalDataSourceImpl
 import com.example.news.data.datasource.NewsDataSource
+import com.example.news.data.firebase.FirebaseDataSourceImpl
 import org.koin.dsl.module
 
 val dataSourceModule = module {
@@ -12,5 +13,12 @@ val dataSourceModule = module {
 
     single<NewsDataSource.Local> {
         NewsLocalDataSourceImpl(newsDao = get())
+    }
+
+    single<NewsDataSource.Firebase> {
+        FirebaseDataSourceImpl(
+            firebaseUser = get(),
+            firebaseDatabase = get()
+        )
     }
 }
