7cd24935f605995d28f5fad6f828b6cf832fe795
Vitalii <movsesyan,vitaliy24ua@gmail.com>
2022-09-22 14:59:43 +0300

    Impl ProfileViewModel.kt

--- app/src/main/java/com/example/news/di/ViewModelModule.kt
+++ app/src/main/java/com/example/news/di/ViewModelModule.kt
@@ -1,11 +1,16 @@
 package com.example.news.di
 
 import com.example.news.ui.tabs.news_list.NewsViewModel
+import com.example.news.ui.tabs.profile.ProfileViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModel {
         NewsViewModel(getNewsListUseCase = get())
+        ProfileViewModel(
+            getFirebaseUserUseCase = get(),
+            setFirebaseUserUseCase = get()
+        )
     }
 }
--- app/src/main/java/com/example/news/ui/tabs/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/news/ui/tabs/profile/ProfileViewModel.kt
@@ -1,8 +1,36 @@
 package com.example.news.ui.tabs.profile
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.news.domain.usecase.GetFirebaseUserUseCase
+import com.example.news.domain.usecase.SetFirebaseUserUseCase
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.coroutines.launch
 
 class ProfileViewModel(
-
+    private val getFirebaseUserUseCase: GetFirebaseUserUseCase,
+    private val setFirebaseUserUseCase: SetFirebaseUserUseCase
 ) : ViewModel() {
+
+    private val _firebaseUser = MutableLiveData<FirebaseUser>()
+    val firebaseUser: LiveData<FirebaseUser> = _firebaseUser
+
+    init {
+        getUser()
+    }
+
+    fun getUser() {
+        viewModelScope.launch {
+            _firebaseUser.value = getFirebaseUserUseCase.execute()
+        }
+    }
+
+    fun setUser(user: FirebaseUser) {
+        viewModelScope.launch {
+            setFirebaseUserUseCase.execute(user)
+            getUser()
+        }
+    }
 }
