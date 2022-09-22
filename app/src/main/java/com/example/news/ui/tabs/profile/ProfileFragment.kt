6343f10dbb08439b86ca9fe7280e35fdf205d663
Vitalii <movsesyan,vitaliy24ua@gmail.com>
2022-09-22 15:30:48 +0300

    Refactor

--- app/src/main/java/com/example/news/ui/tabs/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/news/ui/tabs/profile/ProfileFragment.kt
@@ -11,18 +11,21 @@ import com.bumptech.glide.Glide
 import com.example.news.R
 import com.example.news.databinding.FragmentProfileBinding
 import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.ktx.app
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentProfileBinding
+    private val profileViewModel by viewModel<ProfileViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -36,20 +39,29 @@ class ProfileFragment : Fragment() {
 
     private fun updateUi() {
         Glide.with(binding.userImage.context)
-            .load(firebaseUser?.photoUrl)
+            .load(user()?.photoUrl)
             .circleCrop()
             .into(binding.userImage)
 
-        binding.userName.text = firebaseUser?.displayName
-        binding.userMail.text = firebaseUser?.email
+        binding.userName.text = user()?.displayName
+        binding.userMail.text = user()?.email
     }
 
     private fun logOut() {
         binding.signOutButton.setOnClickListener {
             AuthUI.getInstance().signOut(requireContext()).addOnSuccessListener {
-                firebaseUser = Firebase.auth.currentUser
+                profileViewModel.setUser(Firebase.auth.currentUser!!)
                 findNavController().navigate(R.id.signInFragment)
             }
         }
+    }
+
+    private fun user(): FirebaseUser? {
+        var user: FirebaseUser? = null
+        profileViewModel.firebaseUser.observe(viewLifecycleOwner) {
+            user = it
+        }
+
+        return user
     }
 }
