ad792ff8c0fccc7938a599966e5c3dfc292a0eae
Vitalii <movsesyan,vitaliy24ua@gmail.com>
2022-09-22 16:05:01 +0300

    Refactor NewsListFragment.kt

--- app/src/main/java/com/example/news/ui/tabs/news_list/NewsListFragment.kt
+++ app/src/main/java/com/example/news/ui/tabs/news_list/NewsListFragment.kt
@@ -1,7 +1,6 @@
 package com.example.news.ui.tabs.news_list
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,14 +9,10 @@ import android.widget.Toast
 import androidx.core.app.ShareCompat
 import androidx.navigation.fragment.NavHostFragment
 import com.example.news.R
-import com.example.news.data.objects.CategoriesData
 import com.example.news.data.objects.RequestParam
 import com.example.news.databinding.FragmentNewsListBinding
 import com.example.news.domain.model.NewsDomainModel
-import com.example.news.ui.news_detail.NewsDetailFragment
 import com.example.news.ui.tabs.TabsFragmentDirections
-import com.example.news.ui.tabs.profile.firebaseUser
-import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -31,8 +26,6 @@ class NewsListFragment : Fragment(),
     private val adapterCategories = NewsCategoriesAdapter(this)
     private val newsViewModel by viewModel<NewsViewModel>()
 
-    private val database = Firebase.database
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
@@ -45,6 +38,7 @@ class NewsListFragment : Fragment(),
 
         initRecycler()
         showNews()
+        showToastWithResult()
     }
 
     private fun initRecycler() {
@@ -67,6 +61,14 @@ class NewsListFragment : Fragment(),
         }
     }
 
+    private fun showToastWithResult() {
+        newsViewModel.result.observe(viewLifecycleOwner) {
+            if(it.isNotEmpty()) {
+                Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
     override fun onItemNewsClick(url: String) {
         val topLevelHost = requireActivity().supportFragmentManager
             .findFragmentById(R.id.fragmentContainer) as NavHostFragment
@@ -83,21 +85,7 @@ class NewsListFragment : Fragment(),
     }
 
     override fun onAddBookmarkClick(news: NewsDomainModel) {
-
-/*        val myRef = firebaseUser?.uid?.let {
-            database.reference.child("users").child(it).child("bookmarksNews")
-        }
-
-        if (firebaseUser == null)
-            Toast.makeText(requireContext(), "Sign in account", Toast.LENGTH_SHORT).show()
-        else {
-            myRef?.push()?.setValue(news)?.addOnFailureListener {
-                Toast.makeText(requireContext(), "Failure: ${it.message}", Toast.LENGTH_SHORT).show()
-            }
-                ?.addOnSuccessListener {
-                    Toast.makeText(requireContext(), "News add to bookmark", Toast.LENGTH_SHORT).show()
-                }
-        }*/
+        newsViewModel.addToBookmarks(news)
     }
 
     override fun onItemCategoryClick(category: String) {
